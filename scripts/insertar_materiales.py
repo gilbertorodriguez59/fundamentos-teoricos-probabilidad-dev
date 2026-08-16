382735f3c291924b8346121118a625810ca82599
gilbertorodriguez59 <59020652+gilbertorodriguez59@users.noreply.github.com>
2026-08-15 19:52:59 -0600

    Extiende materiales físicos teóricos al capítulo 11

--- scripts/insertar_materiales.py
+++ scripts/insertar_materiales.py
@@ -11,6 +11,7 @@ CAPITULOS = {
     "08-variables-discretas-pmf.qmd": ("08-variables-discretas-pmf-colab.ipynb", "08-variables-discretas-pmf.Rmd"),
     "09-variables-continuas-densidad.qmd": ("09-variables-continuas-densidad-colab.ipynb", "09-variables-continuas-densidad.Rmd"),
     "10-esperanza-varianza-chebyshev.qmd": ("10-esperanza-varianza-chebyshev-colab.ipynb", "10-esperanza-varianza-chebyshev.Rmd"),
+    "11-distribuciones-discretas-especiales.qmd": ("11-distribuciones-discretas-especiales-colab.ipynb", "11-distribuciones-discretas-especiales.Rmd"),
 }
 
 BASE = "https://github.com/gilbertorodriguez59/introduccion-probabilidad-r-geogebra-dev"
